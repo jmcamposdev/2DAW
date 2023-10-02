7700b590ce551af8c77fa3ebf571c96ff869341e
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-02 08:55:24 +0200

    Added UD1 ej9 HCL

---
+++ HCL/UD1/ej9/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercise</title>
+</head>
+<body>
+  <?php
+    $pesetas = 135632;
+    $euros = $pesetas / 166.386;
+
+    echo "$pesetas pesetas son $euros euros";
+
+    ?>
+</body>
+</html>
