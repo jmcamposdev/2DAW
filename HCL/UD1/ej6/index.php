5c66e56c00f420acd8b1350b7d67f869cfc8448b
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-02 08:54:41 +0200

    Added UD1 ej6 HCL

---
+++ HCL/UD1/ej6/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercise</title>
+  <style>
+    table,tr,td {
+      border: 1px solid black;
+    }
+  </style>
+</head>
+<body>
+  <?php
+    $nombre = "Jose María Campos Trujillo";
+    echo "<h1>$nombre</h1>";
+  ?>
+</body>
+</html>
