68b71abe8aa05d40e4f095add7bdc006453f1766
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-02 08:38:35 +0200

    Added UD1 ej2 HCL

---
+++ HCL/UD1/ej2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Exercise</title>
+</head>
+<body>
+  <?php
+    echo "<h1>José María Campos Trujillo</h1>";
+    echo "<h2>Avenida Dílar</h2>";
+    echo "<b>+34 623414911</b>"
+    ?>
+</body>
+</html>
