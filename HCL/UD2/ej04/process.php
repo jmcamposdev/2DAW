762bf3cd1fd71a9f505f90381fb70f80b4483186
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-03 08:38:53 +0200

    Added UD2 ej04 HCL

---
+++ HCL/UD2/ej04/process.php
@@ -0,0 +1,10 @@
+<?php
+  $numero1 = $_POST['numero1'];
+  $numero2 = $_POST['numero2'];
+
+  echo "La suma de $numero1 + $numero2 es: " . $numero1 + $numero2 . "<br>";
+  echo "La resta de $numero1 - $numero2 es: " . $numero1 - $numero2 . "<br>";
+  echo "La multiplicación de $numero1 * $numero2 es: " . $numero1 * $numero2 . "<br>";
+  echo "La división de $numero1 / $numero2 es: " . $numero1 / $numero2 . "<br>";
+
+?>
