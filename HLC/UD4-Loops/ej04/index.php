09b0eac3f22c8469dab324e475c22a2bce899dbe
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-07 19:16:50 +0200

    Added HLC UD4 ej04

---
+++ HLC/UD4-Loops/ej04/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ejercicio 04</title>
+</head>
+
+<body>
+  <?php
+  // Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle for.
+
+  for ($i=320; $i >= 160; $i-=20) { 
+    echo "$i <br>";
+  }
+  ?>
+</body>
+
+</html>
