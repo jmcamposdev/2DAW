181717433466985fc3afc36bb0c7664e1825befe
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-21 18:56:06 +0000

    Add getUsersStmt method to Test class

--- DWECL/UD3/1.CRUD/classes/test.class.php
+++ DWECL/UD3/1.CRUD/classes/test.class.php
@@ -16,5 +16,23 @@ class Test extends Dbh {
     }
   }
 
+    /**
+   * Get users with prepared statements 
+   * Filter by name and phone
+   */
+  public function getUsersStmt(string $nombreUsuario, string $telefono) {
+    // Create the prepared statement
+    $sql = "SELECT * FROM USUARIOS WHERE nombreusuario = ? AND telefono = ?";
+    // Prepare the statement
+    $stmt = $this->connect()->prepare($sql);
+    // Execute the prepared statement and pass the parameters as an array 
+    $stmt->execute([$nombreUsuario, $telefono]);
+    // Fetch the results as an associative array
+    $names = $stmt->fetchAll();
 
+    // Loop through the associative array and print the results
+    foreach ($names as $name) {
+      echo $name['nombreusuario'] . ' ' . $name['telefono'];
+    }
+  }
 }
