1bd512e19fa692b0b22baef8a313d63b0bbbfae5
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-22 09:29:40 +0000

    Remove unused code and comments

--- DWESE/UD3/1.CRUD/index.php
+++ DWESE/UD3/1.CRUD/index.php
@@ -11,10 +11,6 @@
 </head>
 <body>
 <?php
-    $testObj = new Test();
-    //$testObj->getUsers();
-    //$testObj->getUsersStmt('Paco', '666666666');
-    //$testObj->setUsersStmt('Paco', '1234', '666666666', '2020-12-12');
     // $userObj = new UsersView();
     // $userObj->showUser("Antonio Bueno");
 
