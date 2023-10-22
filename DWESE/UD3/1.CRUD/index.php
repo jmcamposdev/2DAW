3a8898bcf86220f56462b7392d07e92902758e13
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-22 17:14:57 +0000

    Refactored book filtering and added create book functionality

--- DWESE/UD3/1.CRUD/index.php
+++ DWESE/UD3/1.CRUD/index.php
@@ -27,60 +27,74 @@ session_start();
         <div class="separator"></div>
       </div>
       <div class="table__container">
-      <?php
-      // If we don't have the category in the session, we show all the books
-      if (!isset($_POST['submitBooksCategories']) && !isset($_SESSION['category'])) {
-        $booksObj = new BooksView();
-        echo $booksObj->showBooks();
-      } else {
-        if (isset($_POST['submitBooksCategories'])) {
-          $category = $_POST['category'];
-          $_SESSION['category'] = $category;
-        } else {
-          $category = $_SESSION['category'];
-        }
-
-        if ($category == "all") {
+        <?php
+        // If we don't have the category in the session, we show all the books
+        if (!isset($_POST['submitBooksCategories']) && !isset($_SESSION['category'])) {
           $booksObj = new BooksView();
           echo $booksObj->showBooks();
         } else {
-          $booksObj = new BooksView();
-          echo $booksObj->showBooksByCategory($category);
-        }
-      }
-      ?>
-      <div class="filter__container">
-        <h3>Filter</h3>
-        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-          <select name="category" id="category">
-            <option value="all">All</option>
-            <?php
-            foreach (BooksView::$categories as $category) {
-              if (isset($_SESSION['category']) && $_SESSION['category'] == $category) {
-                echo "<option value='$category' selected>". ucfirst($category). "</option>";
-              } else if (isset($_POST['submitBooksCategories']) && $_POST['category'] == $category) {
-                echo "<option value='$category' selected>". ucfirst($category). "</option>";
-              } else {
-                echo "<option value='$category'>". ucfirst($category). "</option>";
-              }
-            }
-            ?>
-          </select>
-          <input type="submit" name="submitBooksCategories" value="Filter">
-        </form>
-      </div>
-    </div>
-  </section>
+          if (isset($_POST['submitBooksCategories'])) {
+            $category = $_POST['category'];
+            $_SESSION['category'] = $category;
+          } else {
+            $category = $_SESSION['category'];
+          }
 
-    <?php 
+          if ($category == "all") {
+            $booksObj = new BooksView();
+            echo $booksObj->showBooks();
+          } else {
+            $booksObj = new BooksView();
+            echo $booksObj->showBooksByCategory($category);
+          }
+        }
+        ?>
+        <div class="books__action__container">
+          <div class="filter__container">
+            <h3>Filter</h3>
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+              <label for="category">Category</label>
+              <select name="category" id="category">
+                <option value="all">All</option>
+                <?php
+                foreach (BooksView::$categories as $category) {
+                  if (isset($_SESSION['category']) && $_SESSION['category'] == $category) {
+                    echo "<option value='$category' selected>" . ucfirst($category) . "</option>";
+                  } else if (isset($_POST['submitBooksCategories']) && $_POST['category'] == $category) {
+                    echo "<option value='$category' selected>" . ucfirst($category) . "</option>";
+                  } else {
+                    echo "<option value='$category'>" . ucfirst($category) . "</option>";
+                  }
+                }
+                ?>
+              </select>
+              <input type="submit" name="submitBooksCategories" value="Filter">
+            </form>
+          </div>
+          <div class="create__container">
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+              <input type="submit" name="submitCreateBook" value="Create New Book">
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php
     if (isset($_POST['submitEditBook'])) {
       $id = $_POST['id'];
       $booksObj = new BooksView();
       echo $booksObj->showFormEditBook($id);
     }
     ?>
-</main>
-<script src="popup.js"></script>
+    <?php
+    if (isset($_POST['submitCreateBook'])) {
+      $booksObj = new BooksView();
+      echo $booksObj->showCreateBookForm();
+    }
+    ?>
+  </main>
+  <script src="popup.js"></script>
 </body>
 
 </html>
