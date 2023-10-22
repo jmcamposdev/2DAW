8228bd2e0a206750711d779dc269492f452f8323
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-22 11:35:18 +0000

    Add styles.css file to index.php

--- DWESE/UD3/1.CRUD/index.php
+++ DWESE/UD3/1.CRUD/index.php
@@ -9,6 +9,7 @@ include "includes/class-autoload.inc.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Library</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
