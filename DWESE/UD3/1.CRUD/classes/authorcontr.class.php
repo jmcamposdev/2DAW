7894077e51f5596d52ba720edceb6c6e0f6f0a76
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-22 16:51:33 +0000

    Add existsAuthor method to AuthorContr class

--- DWESE/UD3/1.CRUD/classes/authorcontr.class.php
+++ DWESE/UD3/1.CRUD/classes/authorcontr.class.php
@@ -4,4 +4,15 @@ class AuthorContr extends Author {
   public function getId($name, $surname) {
     return $this->getIdFromAuthor($name, $surname);
   }
+
+  public function existsAuthor($id) {
+    $result = $this->getAuthorById($id);
+    
+    if ($result) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 }
