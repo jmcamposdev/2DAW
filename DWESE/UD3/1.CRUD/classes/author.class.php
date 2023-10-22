8a44ccd2992345449d65a4b643f78f2d0ab4088f
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-22 16:51:14 +0000

    Add methods to retrieve authors from the database by ID and all authors.

--- DWESE/UD3/1.CRUD/classes/author.class.php
+++ DWESE/UD3/1.CRUD/classes/author.class.php
@@ -4,6 +4,25 @@
  * Class to manage the authors
  */
 class Author extends Dbh {
+  /**
+   * Return all the authors from the DB
+   */
+  protected function getAuthors() {
+    $sql = "SELECT * FROM AUTORES";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $result;
+  }
+
+  protected function getAuthorById($id) {
+    $sql = "SELECT * FROM AUTORES WHERE id_autor = ?";
+    $stmt = $this->connect()->prepare($sql);
+    $stmt->execute([$id]);
+    $result = $stmt->fetch();
+    return $result;
+  }
+
 
   /**
    * Return the id of an author with the name and surname passed as parameters
