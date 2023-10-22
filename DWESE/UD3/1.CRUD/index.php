cdd71e49ee0bc07911d61aa3c9bfd38497c90ae1
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-22 11:34:14 +0000

    Update header and fix formatting in index.php

--- DWESE/UD3/1.CRUD/index.php
+++ DWESE/UD3/1.CRUD/index.php
@@ -1,18 +1,23 @@
 <?php
-  include "includes/class-autoload.inc.php";
+include "includes/class-autoload.inc.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Library</title>
 </head>
+
 <body>
   <main>
-    <h1>Library</h1>
-    <h2>All Content</h2>
+    <header class="header">
+      <div class="logo">
+        <h1>LIBRARY/&gt;</h1>
+      </div>
+    </header>
 
     <section class="content">
       <h2>Books</h2>
@@ -24,35 +29,36 @@
             <th>Author</th>
             <th>Description</th>
           </tr>
-        </thead>            
-          <tbody>
-            <tr>
-              <td>The Great Gatsby</td>
-              <td>Fiction</td>
-              <td>F. Scott Fitzgerald</td>
-              <td>A story about the decadence of the Jazz Age</td>
-            </tr>
-            <tr>
-              <td>To Kill a Mockingbird</td>
-              <td>Fiction</td>
-              <td>Harper Lee</td>
-              <td>A story about racial injustice in the South</td>
-            </tr>
-            <tr>
-              <td>1984</td>
-              <td>Fiction</td>
-              <td>George Orwell</td>
-              <td>A dystopian novel about a totalitarian government</td>
-            </tr>
-            <tr>
-              <td>Pride and Prejudice</td>
-              <td>Fiction</td>
-              <td>Jane Austen</td>
-              <td>A romantic novel about the social norms of the Regency era</td>
-            </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Great Gatsby</td>
+            <td>Fiction</td>
+            <td>F. Scott Fitzgerald</td>
+            <td>A story about the decadence of the Jazz Age</td>
+          </tr>
+          <tr>
+            <td>To Kill a Mockingbird</td>
+            <td>Fiction</td>
+            <td>Harper Lee</td>
+            <td>A story about racial injustice in the South</td>
+          </tr>
+          <tr>
+            <td>1984</td>
+            <td>Fiction</td>
+            <td>George Orwell</td>
+            <td>A dystopian novel about a totalitarian government</td>
+          </tr>
+          <tr>
+            <td>Pride and Prejudice</td>
+            <td>Fiction</td>
+            <td>Jane Austen</td>
+            <td>A romantic novel about the social norms of the Regency era</td>
+          </tr>
         </tbody>
       </table>
     </section>
   </main>
 </body>
+
 </html>
