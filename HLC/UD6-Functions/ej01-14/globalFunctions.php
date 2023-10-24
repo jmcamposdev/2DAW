8911d38544acc7778503faf2af636e0a7cba14df
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-24 10:51:21 +0000

    Add function to check if a number is prime

--- HLC/UD6-Functions/ej01-14/globalFunctions.php
+++ HLC/UD6-Functions/ej01-14/globalFunctions.php
@@ -10,5 +10,23 @@ function esCapicua(int $number) {
   return $number == $reverseNumber;
 }
 
+/**
+ * Comprueba si un número es primo
+ * @param int $number Número a comprobar
+ * @return bool True si es primo, false si no
+ */
+function esPrimo(int $number) {
+  // Inicializamos la variable a true
+  $esPrimo = true;
+  // Recorremos todos los números desde el 2 hasta el número anterior
+  for ($i=2; $i < $number && $esPrimo; $i++) { 
+    // Si el resto de dividir el número entre el contador es 0, no es primo
+    if ($number % $i === 0) {
+      $esPrimo = false;
+    }
+  }
+  // Devolvemos true o false según si es primo o no
+  return $esPrimo;
+}
 
 
