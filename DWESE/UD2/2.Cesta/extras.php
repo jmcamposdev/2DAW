8c416014bd600835527715dcf6a0c5615b7997b0
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-10-28 17:15:26 +0000

    Refactor navigation and layout in secondPlate.php and result.php

--- DWESE/UD2/2.Cesta/extras.php
+++ DWESE/UD2/2.Cesta/extras.php
@@ -134,80 +134,81 @@ if (isset($_POST['quantityFormSubmitMinus'])) {
 </head>
 
 <body>
-<?php
-include_once "./assets/header.php";
-?>
-<section class="nav__container">
-  <div class="arrow__container">
-    <a href="secondPlate.php" class="arrow__wrapper">
-      <img class="left-arrow" src="./img/arrow.svg" alt="Right Arrow">
-      <span>Previous</span>
-    </a>
-  </div>
-  <div class="title">
-    <h1>Extras</h1>
-  </div>
-  <div class="arrow__container">
-    <a href="result.php" class="arrow__wrapper">
-      <span>Finish</span>
-      <img class="right-arrow" src="./img/arrow.svg" alt="Right Arrow">
-    </a>
-  </div>
-</section>
-<section class="error_container">
   <?php
-  if (isset($_GET['error'])) {
-    echo $_GET['error'];
-  }
+  include_once "./assets/header.php";
   ?>
-</section>
-<section class="productos__wrapper">
-  <div class="productos__container">
+  <section class="nav__container">
+    <div class="arrow__container">
+      <a href="secondPlate.php" class="arrow__wrapper">
+        <img class="left-arrow" src="./img/arrow.svg" alt="Right Arrow">
+        <span>Previous</span>
+      </a>
+    </div>
+    <div class="title">
+      <h1>Extras</h1>
+    </div>
+    <div class="arrow__container">
+      <a href="result.php" class="arrow__wrapper">
+        <span>Finish</span>
+        <img class="right-arrow" src="./img/arrow.svg" alt="Right Arrow">
+      </a>
+    </div>
+  </section>
+  <section class="error_container">
     <?php
-    foreach (products as $product) {
-      $productQuantity = 1;
-      foreach ($_SESSION['cart']['extras'] as $producto => $value) {
-        if ($value['id'] == $product['id']) {
-          $productQuantity = $value['quantity'];
-        }
-      }
-      $buttonText = in_array($product['id'], $_SESSION['extrasSelectedProducts']) ? "<img alt='Check icon' src='./img/check.svg'/ '>" : "Añadir";
-      echo "<div class='producto__wrapper'>";
-      echo "  <div class='producto'>";
-      echo "    <img src='" . $product['image'] . "' alt='Producto'>";
-      echo "    <h3>" . $product['name'] . "</h3>";
-      echo "    <p class='producto__precio'>" . $product['price'] . "€</p>";
-      echo "    <div class='quantity__container " . (in_array($product['id'], $_SESSION['extrasSelectedProducts']) ? "" : "quantity__container-hidden") . "'>";
-      echo "      <form class='quantity__container' action='" . $_SERVER['PHP_SELF'] . "' method='POST'>";
-      echo '        <button  name="quantityFormSubmitMinus" type="submit" ><svg class="minus" fill="#000000d1" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M0 12v1h23v-1h-23z"></path></svg></button>';
-      echo "        <input type='hidden' name='id' value='" . $product['id'] . "'/>";
-      echo "        <input name='quantity' type='number' min='1' max='5' value='$productQuantity'/>";
-      echo '        <button name="quantityFormSubmitPlus" type="submit"><svg fill="#000000d1" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11 11v-11h1v11h11v1h-11v11h-1v-11h-11v-1h11z"></path></svg></button>';
-      echo "      </form>";
-      echo "    </div>";
-      echo "    <form action='" . $_SERVER['PHP_SELF'] . "' method='POST'>";
-      echo "      <input type='hidden' name='id' value='" . $product['id'] . "'/>";
-      echo "      <input type='hidden' name='productName' value='" . $product['name'] . "'/>";
-      echo "      <input type='hidden' name='productPrice' value='" . $product['price'] . "'/>";
-      echo "      <button name='productFormSubmit' class='producto__btn'>$buttonText</button>";
-      echo "    </form>";
-      echo "  </div>";
-      echo "</div>";
+    if (isset($_GET['error'])) {
+      echo $_GET['error'];
     }
     ?>
-  </div>
-</section>
-<!-- Borrar la Sesión -->
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-  <input type="hidden" name="borrar" value="1">
-  <button class="borrar__btn">Borrar</button>
-</form>
-<?php
-if (isset($_POST['borrar'])) {
-  if (isset($_SESSION['cart'])) {
-    session_destroy();
+  </section>
+  <section class="productos__wrapper">
+    <div class="productos__container">
+      <?php
+      foreach (products as $product) {
+        $productQuantity = 1;
+        foreach ($_SESSION['cart']['extras'] as $producto => $value) {
+          if ($value['id'] == $product['id']) {
+            $productQuantity = $value['quantity'];
+          }
+        }
+        $buttonText = in_array($product['id'], $_SESSION['extrasSelectedProducts']) ? "<img alt='Check icon' src='./img/check.svg'/ '>" : "Añadir";
+        echo "<div class='producto__wrapper'>";
+        echo "  <div class='producto'>";
+        echo "    <img src='" . $product['image'] . "' alt='Producto'>";
+        echo "    <h3>" . $product['name'] . "</h3>";
+        echo "    <p class='producto__precio'>" . $product['price'] . "€</p>";
+        echo "    <div class='quantity__container " . (in_array($product['id'], $_SESSION['extrasSelectedProducts']) ? "" : "quantity__container-hidden") . "'>";
+        echo "      <form class='quantity__container' action='" . $_SERVER['PHP_SELF'] . "' method='POST'>";
+        echo '        <button  name="quantityFormSubmitMinus" type="submit" ><svg class="minus" fill="#000000d1" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M0 12v1h23v-1h-23z"></path></svg></button>';
+        echo "        <input type='hidden' name='id' value='" . $product['id'] . "'/>";
+        echo "        <input name='quantity' type='number' min='1' max='5' value='$productQuantity'/>";
+        echo '        <button name="quantityFormSubmitPlus" type="submit"><svg fill="#000000d1" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M11 11v-11h1v11h11v1h-11v11h-1v-11h-11v-1h11z"></path></svg></button>';
+        echo "      </form>";
+        echo "    </div>";
+        echo "    <form action='" . $_SERVER['PHP_SELF'] . "' method='POST'>";
+        echo "      <input type='hidden' name='id' value='" . $product['id'] . "'/>";
+        echo "      <input type='hidden' name='productName' value='" . $product['name'] . "'/>";
+        echo "      <input type='hidden' name='productPrice' value='" . $product['price'] . "'/>";
+        echo "      <button name='productFormSubmit' class='producto__btn'>$buttonText</button>";
+        echo "    </form>";
+        echo "  </div>";
+        echo "</div>";
+      }
+      ?>
+    </div>
+  </section>
+  <!-- Borrar la Sesión -->
+  <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="hidden" name="borrar" value="1">
+    <button class="borrar__btn">Borrar</button>
+  </form>
+  <?php
+  if (isset($_POST['borrar'])) {
+    if (isset($_SESSION['cart'])) {
+      session_destroy();
+    }
   }
-}
-?>
+  ?>
 </body>
+
 </html>
