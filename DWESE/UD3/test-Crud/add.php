125400454d58db16405ea85b9734733ad0e2b845
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-11-05 22:22:51 +0000

    Fix database connection error in add.php

--- DWESE/UD3/test-Crud/add.php
+++ DWESE/UD3/test-Crud/add.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Add</title>
 </head>
+
 <body>
   <form action="add.php">
-    
+
   </form>
 </body>
+
 </html>
 <?php
 
@@ -18,8 +21,8 @@ $dwes = new mysqli('localhost', 'dwes', 'abc123.', 'dwes');
 
 // Comprobamos la conexión
 if ($dwes->connect_errno != null) {
-    echo "ERROR: $dwes->connect_error";
-    exit(); // Deja de ejecutar la página
+  echo "ERROR: $dwes->connect_error";
+  exit(); // Deja de ejecutar la página
 }
 
 // Seleccionamos la base de datos
