43ab05adfa812dba117f5614e596e25dae8a95e5
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-11-05 22:22:24 +0000

    Refactor HTML and PHP code for prepared queries

--- DWESE/UD3/consultasPreparadas/index.php
+++ DWESE/UD3/consultasPreparadas/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@
       margin: 0 auto;
       margin-bottom: 200px;
     }
+
     table {
       border-collapse: collapse;
       max-width: 700px;
@@ -17,9 +19,10 @@
     }
   </style>
 </head>
+
 <body>
   <form action="index.php" method="post">
-  <h3>Rango de Precios</h3>
+    <h3>Rango de Precios</h3>
     <label for="min">Precio Mínimo</label>
     <input type="number" name="min" id="min" min="0" step="0.01" required>
     <label for="max">Precio Máximo</label>
@@ -27,23 +30,24 @@
     <input type="submit" value="Buscar">
   </form>
 </body>
+
 </html>
 <?php
 
 // Comprobamos que se han recibido los datos 
 if (!isset($_POST['min']) || !isset($_POST['max']) || $_POST['min'] == '' || $_POST['max'] == '') {
-    exit();
+  exit();
 } else if ($_POST['min'] > $_POST['max']) {
-    echo "El precio mínimo no puede ser mayor que el precio máximo";
-    exit();
+  echo "El precio mínimo no puede ser mayor que el precio máximo";
+  exit();
 }
 
 // Conexión a la base de datos
 $dwes = new mysqli('localhost', 'dwes', 'abc123.', 'dwes');
 // Comprobamos la conexión
 if ($dwes->connect_errno != null) {
-    echo "ERROR: $dwes->connect_error";
-    exit(); // Deja de ejecutar la página
+  echo "ERROR: $dwes->connect_error";
+  exit(); // Deja de ejecutar la página
 }
 
 // Seleccionamos la base de datos
@@ -63,17 +67,17 @@ $productos = $resultado->fetch_array();
 
 // Si hay datos, mostramos la tabla
 if ($productos != null) {
-    // Mostramos la cabecera de la tabla
-    echo "<table border='1'><tr><th>Nombre Corto</th><th>Descripción</th><th>PVP</th><th>Familia</th></tr>";
-    while ($productos != null) {
-        // Mostramos los datos de cada fila
-        echo "<tr><td>$productos[0]</td><td>$productos[1]</td><td>$productos[2]</td><td>$productos[3]</td></tr>";
-        $productos = $resultado->fetch_array();
-    }
-    // Cerramos la tabla
-    echo "</table>";
+  // Mostramos la cabecera de la tabla
+  echo "<table border='1'><tr><th>Nombre Corto</th><th>Descripción</th><th>PVP</th><th>Familia</th></tr>";
+  while ($productos != null) {
+    // Mostramos los datos de cada fila
+    echo "<tr><td>$productos[0]</td><td>$productos[1]</td><td>$productos[2]</td><td>$productos[3]</td></tr>";
+    $productos = $resultado->fetch_array();
+  }
+  // Cerramos la tabla
+  echo "</table>";
 } else { // Si no hay datos, mostramos un mensaje
-    echo "No hay datos en la base de datos";
+  echo "No hay datos en la base de datos";
 }
 
 // Cerramos la conexión
