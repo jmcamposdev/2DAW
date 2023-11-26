0330bda44ee91f366abb9f6e6e2ae92769189150
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-11-26 12:34:27 +0000

    Update chirps index.blade.php

--- DWESE/UD4/example-app/resources/views/chirps/index.blade.php
+++ DWESE/UD4/example-app/resources/views/chirps/index.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("Chirps Form") }}
+                    <form method="POST">
+                        <textarea class="bg-transparent"  name="message" id="message" cols="30" rows="10"></textarea>
+                        <input type="submit" value="Send">
+                    </form>
                 </div>
             </div>
         </div>
