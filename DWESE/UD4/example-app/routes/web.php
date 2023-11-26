93a995980f4b24b22f58cab2914aff9fa1432dcd
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-11-26 10:14:20 +0000

    Starting Laravel Test Project

---
+++ DWESE/UD4/example-app/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+Route::get('/chirps', function () {
+    return 'Hello you are in chirps';
+})->name('chirps.index');
+
+Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/chirps', function () {
+        return view('chirps.index');
+    })->name('chirps.index');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
