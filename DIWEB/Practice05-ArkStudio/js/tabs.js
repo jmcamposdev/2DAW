592875a1b059b2a414f838bbab476fa47e1f9f92
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-02 21:16:33 +0000

    Add tab functionality to service and projects sections

---
+++ DIWEB/Practice05-ArkStudio/js/tabs.js
@@ -0,0 +1,49 @@
+// Start when the page is loaded
+window.addEventListener('load', function() {
+  addTabsEventsListeners("service__tabs");
+  addTabsEventsListeners("projects__tabs");
+});
+
+
+function addTabsEventsListeners(containerId) {
+  const tabs = document.querySelectorAll(`#${containerId} .tab__item`);
+  
+  tabs.forEach(tab => {
+    tab.addEventListener('click', function() {
+      // Change the Tabs Layout
+      removeActiveClass(containerId);
+      addActiveClass(tab);
+      // Change the Tabs Content
+      removeActiveContent(containerId);
+      addActiveContent(tab, containerId);
+    });
+  });
+}
+
+function removeActiveClass(containerId) {
+  const tabs = document.querySelectorAll(`#${containerId} .tab__item`);
+  
+  tabs.forEach(tab => {
+    tab.classList.remove('tab__selected');
+  });
+}
+
+function addActiveClass(tab) {
+  tab.classList.add('tab__selected');
+}
+
+function removeActiveContent(containerId) {
+  const tabsContent = document.querySelectorAll(`#${containerId} .tab__panel`);
+  
+  tabsContent.forEach(content => {
+    content.classList.remove('tab__content--selected');
+  });
+}
+
+function addActiveContent(tab, containerId) {
+  const tabId = tab.dataset.id;
+  console.log(tabId);
+  const tabContent = document.querySelector(`#${containerId} .tab__panel[data-id="${tabId}"]`);
+  
+  tabContent.classList.add('tab__content--selected');
+}
