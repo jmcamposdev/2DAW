654d8dc067eb15461c3be8ae388e0ea7479614e4
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-04 10:44:48 +0000

    Added collapse menu functionality

--- DIWEB/Practice05-ArkStudio/js/main.js
+++ DIWEB/Practice05-ArkStudio/js/main.js
@@ -1,5 +1,7 @@
+import NavCollapse from "./navCollapse.js";
 import Slider from "./slider.js";
 
 window.addEventListener('load', function() {
   new Slider("slider");
+  new NavCollapse("nav", "burger-nav-icon", ["cross-icon", "nav__item"]);
 });
---
+++ DIWEB/Practice05-ArkStudio/js/navCollapse.js
@@ -0,0 +1,21 @@
+export default class NavCollapse {
+    constructor(navId, burgerId, closeId) {
+        this.nav = document.getElementById(navId)
+        this.burger = document.getElementById(burgerId)
+
+        // Add events
+        this.burger.addEventListener('click', () => this.collapse())
+
+        // Add events to close elements
+        closeId.forEach(id => {
+            const close = document.querySelectorAll(`.${id}`)
+            close.forEach(element => {
+                element.addEventListener('click', () => this.collapse())
+            })
+        })
+    }
+
+    collapse() {
+        this.nav.classList.toggle('nav-active')
+    }
+}
