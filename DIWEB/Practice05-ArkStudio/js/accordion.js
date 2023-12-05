c8ac05270f96a58c7839da7bc71deec27d38def4
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-05 11:24:36 +0000

    Add accordion functionality and message icon SVG file

---
+++ DIWEB/Practice05-ArkStudio/js/accordion.js
@@ -0,0 +1,25 @@
+export default function accordion() {
+  const accordionItems = document.querySelectorAll('.faq__item');
+  accordionItems.forEach(item => {
+
+    item.addEventListener('click', () => {
+      // Get the open item
+      const openItem = document.querySelector('.faq__item--open');
+      // If there is an open item close it
+      if (openItem && openItem !== item) {
+        openItem.querySelector('.faq__item--open .faq__text').style.height = 0;
+        openItem.classList.remove('faq__item--open');
+      }
+
+      // If the item is already open close it
+      if (item.classList.contains('faq__item--open')) {
+        item.querySelector('.faq__text').style.height = 0;
+        item.classList.remove('faq__item--open');
+      } else {
+        // Open the item
+        item.querySelector('.faq__text').style.height =  (item.querySelector('.faq__text').scrollHeight + 14) + 'px';
+        item.classList.add('faq__item--open');
+      }
+    });
+  });
+}
--- DIWEB/Practice05-ArkStudio/js/main.js
+++ DIWEB/Practice05-ArkStudio/js/main.js
@@ -1,7 +1,9 @@
+import accordion from "./accordion.js";
 import NavCollapse from "./navCollapse.js";
 import Slider from "./slider.js";
 
 window.addEventListener('load', function() {
   new Slider("slider");
   new NavCollapse("nav", "burger-nav-icon", ["cross-icon", "nav__item"]);
+  accordion();
 });
