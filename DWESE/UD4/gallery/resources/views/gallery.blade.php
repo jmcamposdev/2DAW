c07d34fd172d664482b178dfc5a88719cb8f93e1
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-14 10:34:31 +0000

    Add image download button to gallery view

--- DWESE/UD4/gallery/resources/views/gallery.blade.php
+++ DWESE/UD4/gallery/resources/views/gallery.blade.php
@@ -36,7 +36,15 @@
         @foreach ($chunks as $chunk)
           <div class="column">
             @foreach ($chunk as $image)
-              <img src="{{ asset("images/".$image) }}" class="img-fluid" alt="Imagen">
+              <div class="image__container">
+                <a href="{{ asset("images/".$image) }}">
+                  <img src="{{ asset("images/".$image) }}" class="img-fluid" alt="Imagen">
+                </a>
+                <a class="download__button" href="{{ asset("images/".$image) }}" download="">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#f7f2e2" d="m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11l-5 5Zm-6 4q-.825 0-1.413-.588T4 18v-3h2v3h12v-3h2v3q0 .825-.588 1.413T18 20H6Z"/></svg>
+                </a>
+              </div>
+              
             @endforeach
           </div>
         @endforeach
