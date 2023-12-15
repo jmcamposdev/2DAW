8b68e7c7a6a2f1cb0d054687d16edc613036012c
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-15 07:12:46 +0000

    Remove unnecessary validation

--- DWESE/UD4/login/resources/views/dashboard.blade.php
+++ DWESE/UD4/login/resources/views/dashboard.blade.php
@@ -7,14 +7,9 @@
 <body>
     <h1>Welcome to the Dashboard</h1>
 
-    @if(session('username'))
-        <p>Hello, {{ session('username') }}!</p>
-        <!-- Aquí puedes mostrar contenido específico para usuarios logueados -->
-        <p>This is your dashboard content.</p>
-        <a href="{{ url('/logout') }}">Logout</a>
-    @else
-        <p>You are not logged in.</p>
-        <a href="{{ url('/') }}">Login</a>
-    @endif
+    <p>Hello, {{ session('username') }}!</p>
+    <!-- Aquí puedes mostrar contenido específico para usuarios logueados -->
+    <p>This is your dashboard content.</p>
+    <a href="{{ url('/logout') }}">Logout</a>
 </body>
 </html>
