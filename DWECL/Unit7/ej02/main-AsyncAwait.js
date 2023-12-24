5f4d858e01fda55e6dcc895e2de0efc7123f3325
Jose María <jmcamposdev@gmail.com>
2023-12-24 14:24:09 +0000

    Add async function to show completed todos

--- DWECL/Unit7/ej02/main-AsyncAwait.js
+++ DWECL/Unit7/ej02/main-AsyncAwait.js
@@ -13,22 +13,39 @@ function main() {
   btnShowTodos.addEventListener('click', showTodosCompleted);
 }
 
+/**
+ * Async function to show the completed todos.
+ */
 async function showTodosCompleted() {
+  // Get the table body.
   const tableBody = document.querySelector('#table-body');
+  // Get the todos and wait for the response.
   const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  // Convert the response to json.
   const todos = await response.json();
+  // Filter the todos by completed.
   const completed = todos.filter(todo => todo.completed);
+  // Clear the table body.
+  tableBody.innerHTML = '';
+
+  // Create the table rows.
   completed.forEach(todo => {
+    // Create the table row.
     const tr = document.createElement('tr');
+    // Create the table cells.
     const tdId = document.createElement('td');
     tdId.textContent = todo.id;
     const tdTitle = document.createElement('td');
     tdTitle.textContent = todo.title;
     const tdCompleted = document.createElement('td');
     tdCompleted.textContent = todo.completed;
+
+    // Add the cells to the row.
     tr.appendChild(tdId);
     tr.appendChild(tdTitle);
     tr.appendChild(tdCompleted);
+
+    // Add the row to the table body.
     tableBody.appendChild(tr);
   });
 }
