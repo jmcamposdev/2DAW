c741e5a6b03087e50b2b9c35d66523194fc12238
Jose María <jmcamposdev@gmail.com>
2023-12-25 20:08:38 +0000

    Refactor glider initialization in slider.js

--- DIWEB/Practice04-NewZealand/js/slider.js
+++ DIWEB/Practice04-NewZealand/js/slider.js
@@ -1,10 +1,13 @@
+// Get the glider elements
 let glides = document.getElementsByClassName('glide');
+// Create a new glider for each element
   for (let i = 0; i < glides.length; i++) {
     new Glide(glides[i], {
-      slidesToShow: 1,
-      dots: '.glider-dots',
+      slidesToShow: 1, // Show only one slide at a time
+      dots: '.glider-dots', // Add the dots
     }).mount()
   }
+  
   new Glide('.glide', {
     slidesToShow: 1,
     dots: '.glider-dots',
