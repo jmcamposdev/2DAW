eed551c074205c5bf4af7f08bc475913316c5c6e
Jose María <jmcamposdev@gmail.com>
2023-12-27 08:05:34 +0000

    Add Slider and NavCollapse instances, call accordion and navActive functions

--- DIWEB/Practice05-ArkStudio/js/main.js
+++ DIWEB/Practice05-ArkStudio/js/main.js
@@ -4,8 +4,12 @@ import NavCollapse from "./navCollapse.js";
 import Slider from "./slider.js";
 
 window.addEventListener("load", function () {
+  // Create a new instance of Slider class
   new Slider("slider");
+  // Create a new instance of NavCollapse class
   new NavCollapse("nav", "burger-nav-icon", ["cross-icon", "nav__item"]);
+  // Call to accordion function
   accordion();
+  // Call to navActive function
   navActive();
 });
