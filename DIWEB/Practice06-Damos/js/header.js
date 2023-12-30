7693225d22a1217d7bf743ee16d32f829cb2e357
Jose María <jmcamposdev@gmail.com>
2023-12-30 21:18:06 +0000

    First View of Damos Website

---
+++ DIWEB/Practice06-Damos/js/header.js
@@ -0,0 +1,11 @@
+const header = document.getElementById('header');
+// When the user scrolls down 200px from the top of the document, show the button
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) { // 200px from top
+      // Show the header background
+      header.classList.add('header--active');
+  } else {
+      // Change the background color of the header
+      header.classList.remove('header--active');
+  }
+});
---
+++ DIWEB/Practice06-Damos/js/main.js
@@ -0,0 +1,54 @@
+import NavCollapse from "./navCollapse.js";
+
+window.addEventListener('load', function() {
+  // Create a new instance of NavCollapse class
+  new NavCollapse("nav", "burger-nav-icon", ["cross-icon", "nav__item"]);
+  imagesInit();
+  gridInit();
+});
+
+function gridInit() {
+  const items = document.querySelector('.articles__items');
+  const itemsGrid = new Isotope(items, {
+    itemSelector: '.article',
+    masonry: {
+      fitWidth: true,
+      gutter: 20
+    }
+  })
+
+  // Add event listener for filter
+  const filters = document.querySelectorAll('.filter-articles__item');
+
+  filters.forEach(filter => {
+    filter.addEventListener('click', function() {
+      const filterActiveItem = document.querySelector('.filter-articles__item.active');
+      // Remove active class
+      filterActiveItem.classList.remove('active');
+      // Add active class
+      this.classList.add('active');
+
+      // Filter
+      const filterValue = 
+        this.getAttribute('data-filter') === "*" ?
+        '' :
+        `[data-filter="${this.getAttribute('data-filter')}"]`;
+      console.log(filterValue);
+      itemsGrid.arrange({ filter:  filterValue});
+    });
+  });
+
+
+}
+
+function imagesInit() {
+  const images = document.querySelectorAll('.article__image');
+  if (images.length) {
+    images.forEach(image => {
+      const imageItem = image.querySelector('img');
+      const padding = imageItem.naturalHeight / imageItem.naturalWidth * 100;
+      image.style.paddingBottom = `${padding}%`;
+      imageItem.classList.add('init');
+    });
+  }
+}
