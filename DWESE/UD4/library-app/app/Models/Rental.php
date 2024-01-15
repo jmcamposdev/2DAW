0143a689b50e582620bef3a102496bbebd2f8fd8
Jose María <jmcamposdev@gmail.com>
2024-01-15 07:09:18 +0000

    Add Rental model

---
+++ DWESE/UD4/library-app/app/Models/Rental.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rental extends Model
+{
+    use HasFactory;
+
+    protected $table = 'rentals';
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+        'loan_date',
+        'return_date',
+    ];
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
