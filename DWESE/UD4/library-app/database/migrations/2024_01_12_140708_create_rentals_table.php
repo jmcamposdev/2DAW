92173a03713f6c6c0341943dd30ab15bd8c9d285
Jose María <jmcamposdev@gmail.com>
2024-01-15 07:09:41 +0000

    Refactor rentals table migration

--- DWESE/UD4/library-app/database/migrations/2024_01_12_140708_create_rentals_table.php
+++ DWESE/UD4/library-app/database/migrations/2024_01_12_140708_create_rentals_table.php
@@ -12,20 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('rentals', function (Blueprint $table) {
-            $table->id('rental_id');
+            $table->id();
             $table->unsignedBigInteger('book_id');
             $table->unsignedBigInteger('user_id');
             $table->date('loan_date');
             $table->date('return_date')->nullable();
+            $table->timestamps();
             
-            $table->foreign('book_id')->references('id_book')->on('books')->onDelete('NO ACTION')->onUpdate('CASCADE');
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('NO ACTION')->onUpdate('CASCADE');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('NO ACTION')->onUpdate('CASCADE');
             
             $table->index('book_id');
             $table->index('user_id');
             $table->index('loan_date');
             
-            $table->unique(['book_id', 'user_id', 'loan_date']);
+            //$table->unique(['book_id', 'user_id', 'loan_date']);
         });
     }
 
