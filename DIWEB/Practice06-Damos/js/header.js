6540104b054b513da951f52a8ff6f976a0efa182
Jose María <jmcamposdev@gmail.com>
2024-01-19 10:15:41 +0000

    Remove the scroll animation header

--- DIWEB/Practice06-Damos/js/header.js
+++ DIWEB/Practice06-Damos/js/header.js
@@ -1,6 +1,10 @@
 const header = document.getElementById('header');
 // When the user scrolls down 200px from the top of the document, show the button
 window.addEventListener('scroll', () => {
+  if (window.scrollY > 30) {
+    console.log("scrollY > 30");
+    header.classList.remove("wow");
+  }
   if (window.scrollY > 200) { // 200px from top
       // Show the header background
       header.classList.add('header--active');
