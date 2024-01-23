33f608f7a37e0e63c73cc65e89d29caa65bcb046
Jose María <jmcamposdev@gmail.com>
2024-01-23 07:47:07 +0000

    Add some relevant comment to the script

--- DWECL/Unit7/ej02/main.js
+++ DWECL/Unit7/ej02/main.js
@@ -4,29 +4,57 @@
 
 window.addEventListener('load', main);
 
+/**
+ * The main start of the script
+ * Add event listener to the button to show the completed todos
+ */
 function main() {
+  // Get the button
   const btnShowTodos = document.querySelector('#btn-show-todos');
+  // Add event listener
   btnShowTodos.addEventListener('click', showTodosCompleted);
 }
 
 
+/**
+ * Show all the completed todos using Promises
+ */
 function showTodosCompleted() {
+  // Get the table body
   const tableBody = document.querySelector('#table-body');
+  // Remove all the rows
+  tableBody.innerHTML = '';
+
+  // Get the todos form the API
   fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())
-    .then(todos => {
+    .then(response => response.json()) // Convert the response to JSON
+    .then(todos => { // Get the todos
+      // Filter the completed todos
       const completed = todos.filter(todo => todo.completed);
+
+      // Create the rows
       completed.forEach(todo => {
+        // Create the row
         const tr = document.createElement('tr');
+
+        // Create the cell of the ID
         const tdId = document.createElement('td');
         tdId.textContent = todo.id;
+        
+        // Create the cell of the title
         const tdTitle = document.createElement('td');
         tdTitle.textContent = todo.title;
+        
+        // Create the cell of the completed
         const tdCompleted = document.createElement('td');
         tdCompleted.textContent = todo.completed;
+        
+        // Add the cells to the row
         tr.appendChild(tdId);
         tr.appendChild(tdTitle);
         tr.appendChild(tdCompleted);
+
+        // Add the row to the table body
         tableBody.appendChild(tr);
       });
     });
